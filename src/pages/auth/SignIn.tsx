c36deaf4a94ebc6dc3fb4bebf1ffb6cb201d42f6
Altiansyah Fanny <altiansyahf.nutech@gmail.com>
2024-05-15 10:57:21 +0700

    all illustration to AuthLayout

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,12 +1,12 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { SignInImg } from '@/assets/img';
+import AuthLayout from '@/layouts/AuthLayout';
 import { SignInRequest } from '@/model/auth';
 import { signInSchema } from '@/schema/auth';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
 	const navigate = useNavigate();
@@ -23,56 +23,42 @@ export default function SignIn() {
 	};
 
 	return (
-		<div className="w-full grid grid-cols-3 min-h-screen">
-			<div className="hidden bg-muted lg:block col-span-2">
-				<img
-					src={SignInImg}
-					alt="Image"
-					width="1920"
-					height="1080"
-					className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-				/>
-			</div>
-			<div className="flex items-center justify-center py-12">
-				<form
-					className="mx-auto grid w-[350px]"
-					onSubmit={handleSubmit(onSubmit)}
-					autoComplete="off"
-				>
-					<div className="grid gap-2 text-center">
-						<h1 className="text-3xl font-bold">Login</h1>
+		<AuthLayout>
+			<form className="mx-auto grid w-[350px]" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+				<div className="grid gap-2 text-center">
+					<h1 className="text-3xl font-bold">Welcome back</h1>
+					<p className="text-sm text-muted-foreground">Login to continue to your account</p>
+				</div>
+				<div className="grid gap-4 mt-8">
+					<div className="grid gap-2">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							type="email"
+							placeholder="m@example.com"
+							autoComplete="off"
+							{...register('email')}
+						/>
+						{errors.email && <p className="text-red-500">{errors.email.message}</p>}
 					</div>
-					<div className="grid gap-4 mt-8">
-						<div className="grid gap-2">
-							<Label htmlFor="email">Email</Label>
-							<Input
-								id="email"
-								type="email"
-								placeholder="m@example.com"
-								autoComplete="off"
-								{...register('email')}
-							/>
-							{errors.email && <p className="text-red-500">{errors.email.message}</p>}
+					<div className="grid gap-2">
+						<div className="flex items-center">
+							<Label htmlFor="password">Password</Label>
 						</div>
-						<div className="grid gap-2">
-							<div className="flex items-center">
-								<Label htmlFor="password">Password</Label>
-							</div>
-							<Input id="password" type="password" autoComplete="off" {...register('password')} />
-							{errors.password && <p className="text-red-500">{errors.password.message}</p>}
-						</div>
-						<Button type="submit" className="w-full">
-							Login
-						</Button>
+						<Input id="password" type="password" autoComplete="off" {...register('password')} />
+						{errors.password && <p className="text-red-500">{errors.password.message}</p>}
 					</div>
-					<div className="mt-4 text-center text-sm">
-						Don&apos;t have an account? &nbsp;
-						<Link to="/sign-up" className="underline">
-							Sign up
-						</Link>
-					</div>
-				</form>
-			</div>
-		</div>
+					<Button type="submit" className="w-full">
+						Login
+					</Button>
+				</div>
+				<div className="mt-4 text-center text-sm">
+					Don&apos;t have an account? &nbsp;
+					<Link to="/sign-up" className="underline">
+						Sign up
+					</Link>
+				</div>
+			</form>
+		</AuthLayout>
 	);
 }
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,7 +1,7 @@
-import { SignInImg } from '@/assets/img';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import AuthLayout from '@/layouts/AuthLayout';
 import { SignUpRequest } from '@/model/auth';
 import { signUpSchema } from '@/schema/auth';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -23,76 +23,61 @@ export default function SignUp() {
 	};
 
 	return (
-		<div className="w-full grid grid-cols-3 min-h-screen">
-			<div className="hidden bg-muted lg:block col-span-2">
-				<img
-					src={SignInImg}
-					alt="Image"
-					width="1920"
-					height="1080"
-					className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-				/>
-			</div>
-			<div className="flex items-center justify-center py-12">
-				<form
-					className="mx-auto grid w-[350px]"
-					onSubmit={handleSubmit(onSubmit)}
-					autoComplete="off"
-				>
-					<div className="grid gap-2 text-center">
-						<h1 className="text-3xl font-bold">Register</h1>
+		<AuthLayout>
+			<form className="mx-auto grid w-[350px]" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+				<div className="grid gap-2 text-center">
+					<h1 className="text-3xl font-bold">Register</h1>
+				</div>
+				<div className="grid gap-4 mt-8">
+					<div className="grid gap-2">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							type="email"
+							placeholder="m@example.com"
+							{...register('email', { required: true })}
+						/>
+						{errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
 					</div>
-					<div className="grid gap-4 mt-8">
-						<div className="grid gap-2">
-							<Label htmlFor="email">Email</Label>
-							<Input
-								id="email"
-								type="email"
-								placeholder="m@example.com"
-								{...register('email', { required: true })}
-							/>
-							{errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
+					<div className="grid gap-2">
+						<div className="flex items-center">
+							<Label htmlFor="password">Password</Label>
 						</div>
-						<div className="grid gap-2">
-							<div className="flex items-center">
-								<Label htmlFor="password">Password</Label>
-							</div>
-							<Input
-								id="password"
-								type="password"
-								{...register('password', { required: true })}
-								autoComplete="off"
-							/>
-							{errors.password && (
-								<span className="text-red-500 text-sm">{errors.password.message}</span>
-							)}
-						</div>
-						<div className="grid gap-2">
-							<div className="flex items-center">
-								<Label htmlFor="confirmPassword">Confirm Password</Label>
-							</div>
-							<Input
-								id="confirmPassword"
-								type="password"
-								{...register('confirmPassword', { required: true })}
-								autoComplete="off"
-							/>
-							{errors.confirmPassword && (
-								<span className="text-red-500 text-sm">{errors.confirmPassword.message}</span>
-							)}
-						</div>
-						<Button type="submit" className="w-full">
-							Sign Up
-						</Button>
+						<Input
+							id="password"
+							type="password"
+							{...register('password', { required: true })}
+							autoComplete="off"
+						/>
+						{errors.password && (
+							<span className="text-red-500 text-sm">{errors.password.message}</span>
+						)}
 					</div>
-					<div className="mt-4 text-center text-sm">
-						Already have an account? &nbsp;
-						<Link to="/sign-in" className="underline">
-							Sign in
-						</Link>
+					<div className="grid gap-2">
+						<div className="flex items-center">
+							<Label htmlFor="confirmPassword">Confirm Password</Label>
+						</div>
+						<Input
+							id="confirmPassword"
+							type="password"
+							{...register('confirmPassword', { required: true })}
+							autoComplete="off"
+						/>
+						{errors.confirmPassword && (
+							<span className="text-red-500 text-sm">{errors.confirmPassword.message}</span>
+						)}
 					</div>
-				</form>
-			</div>
-		</div>
+					<Button type="submit" className="w-full">
+						Sign Up
+					</Button>
+				</div>
+				<div className="mt-4 text-center text-sm">
+					Already have an account? &nbsp;
+					<Link to="/sign-in" className="underline">
+						Sign in
+					</Link>
+				</div>
+			</form>
+		</AuthLayout>
 	);
 }
