277e7c8d311c785a77d6388814ac9ef6822fef14
Altiansyah Fanny <altiansyahf.nutech@gmail.com>
2024-07-03 14:03:58 +0700

    fix responsive

--- src/pages/landing-page/Footer.tsx
+++ src/pages/landing-page/Footer.tsx
@@ -11,32 +11,34 @@ const Footer = () => {
 				<div className="md:flex-1 lg:w-5/12 md:p-16 p-4 bg-green-200x">
 					<img src={LogoWhite} alt="logo" className="h-[100px] text-white" />
 					<div className="text-white mt-8">
-						<h2 className="text-lg font-bold">Jangan Lupa Kunjungi Kami</h2>
+						<h2 className="text-lg font-bold 3xl:text-2xl">Jangan Lupa Kunjungi Kami</h2>
 						<a
 							href="https://www.google.com/maps/place/Azara+Course/@-6.3163412,106.8180023,17.75z/data=!4m6!3m5!1s0x2e69ee0a9f35b1df:0xf301f16896ffd3bb!8m2!3d-6.3173186!4d106.8186412!16s%2Fg%2F11dyqz17gx?entry"
 							target="_blank"
 							className="flex gap-4 text-white mt-4"
 						>
 							<MapPin className="text-2xl" />
-							<p>Jl. Sagu No. 45, RT 007/ RW 005 Jagakarsa, Jakarta Selatan 12620</p>
+							<p className="3xl:text-lg">
+								Jl. Sagu No. 45, RT 007/ RW 005 Jagakarsa, Jakarta Selatan 12620
+							</p>
 						</a>
 					</div>
 				</div>
 				<div className="md:flex-1 lg:w-3/12 p-4 md:p-16 text-white bg-red-200x">
 					<div>
-						<h2 className="text-lg font-bold">Tautan</h2>
+						<h2 className="text-lg font-bold 3xl:text-2xl">Tautan</h2>
 						{NAVLINK.map((link) => (
 							<a
 								key={link.id}
 								href={`#${link.id}`}
-								className="text-gray-300 text-sm hover:text-custom-green"
+								className="text-gray-300 text-sm 3xl:text-xl hover:text-custom-green"
 							>
 								<h3>{link.name}</h3>
 							</a>
 						))}
 					</div>
 					<div className="mt-8">
-						<h2 className="text-lg font-bold">Follow Kami</h2>
+						<h2 className="text-lg font-bold 3xl:text-2xl">Follow Kami</h2>
 						<div className="flex-1 flex space-x-4 mt-2">
 							<a
 								href="https://instagram.com/azaracourse"
@@ -44,8 +46,8 @@ const Footer = () => {
 								rel="noopener noreferrer"
 								className="flex items-center gap-4"
 							>
-								<FaInstagram className="text-2xl" />
-								<p>@azaracourse</p>
+								<FaInstagram className="text-2xl 3xl:text-3xl" />
+								<p className="3xl:text-xl">@azaracourse</p>
 							</a>
 						</div>
 					</div>
--- src/pages/landing-page/Home.tsx
+++ src/pages/landing-page/Home.tsx
@@ -31,21 +31,21 @@ const Home = () => {
 
 	return (
 		<div id="home" className="">
-			<Carousel className="" plugins={[Autoplay({ delay: 10000 })]} setApi={setApi}>
+			<Carousel className="" plugins={[Autoplay({ delay: 100000 })]} setApi={setApi}>
 				<CarouselContent className="pb-4">
 					<CarouselItem>
 						<div className="bg-yellow-500x carousel-item px-5 md:px-10 xl:px-40">
 							<div className="h-full">
 								<div className="flex items-center bg-red-500x h-full">
-									<div className="flex-1 bg-green-500x w-full lg:max-w-xl lg:text-center lg:mx-auto xl:w-full xl:text-left">
-										<h1 className="text-4xl 2xl:text-5xl leading-snug font-semibold bg-yellow-200x">
+									<div className="flex-1 bg-green-500x w-full lg:max-w-xl lg:text-center lg:mx-auto xl:max-w-3xl xl:w-full xl:text-left">
+										<h1 className="text-4xl 2xl:text-5xl leading-snug font-semibold bg-yellow-200x 3xl:text-6xl">
 											Bimbingan Belajar yang <br className="hidden xl:block" /> Memberikan&nbsp;
 											<br className="hidden xl:block" />
 											<p className="text-custom-green lg:inline">#PerhatianPenuh</p> bagi&nbsp;
 											<br className="hidden xl:block" />
 											Siswa dan Orang Tua
 										</h1>
-										<p className="text-black text-sm 2xl:text-base mt-4">
+										<p className="text-black text-sm 2xl:text-base mt-4 3xl:text-xl">
 											Menyesuaikan dengan kurikulum terbaru di sekolah,{' '}
 											<br className="hidden xl:block" />
 											pemberian konseling untuk pengembangan karakter,{' '}
@@ -56,10 +56,11 @@ const Home = () => {
 											<div className="h-1 w-40 bg-gray-900 mt-4" />
 										</div>
 									</div>
-									<div className="bg-rex-400 hidden xl:block">
+
+									<div className="hidden xl:block">
 										<img
 											src={HeroImage}
-											className="xl:w-[400px] 2xl:w-[500px]"
+											className="xl:w-[400px] 2xl:w-[500px] 3xl:w-[600px]"
 											style={{ borderRadius: '2rem' }}
 										/>
 									</div>
@@ -72,18 +73,18 @@ const Home = () => {
 							<div className="h-full relative flex items-center bg-blue-500x">
 								<img
 									src={HeroImage2}
-									className="hidden xl:block absolute xl:w-[500px] 2xl:w-[650px]  bottom-0 left-40"
+									className="hidden xl:block absolute xl:w-[500px] 2xl:w-[650px] 3xl:w-[800px]  bottom-0 left-40"
 								/>
 								<div className="hidden xl:block bg-[#F3D743B2] h-full w-1/2 rounded-tr-full rounded-br-full" />
 								<div className="flex-1 px-5 md:px-10 xl:px-20 bg-red-200x">
 									<div className="bg-green-500x lg:max-w-xl lg:text-center lg:mx-auto xl:w-full xl:text-right">
 										<img src={PatternHero} className="mb-4 hidden xl:block ml-auto" />
-										<h1 className="text-4xl leading-snug font-semibold 2xl:text-5xl">
+										<h1 className="text-4xl leading-snug font-semibold 2xl:text-5xl 3xl:text-6xl">
 											Kesempatan <br className="hidden xl:block" /> <span>Daftar Khusus </span>
 											<br className="hidden xl:block" />
 											<p className="text-custom-green lg:inline">#GRATIS</p>
 										</h1>
-										<p className="text-black text-sm 2xl:text-base">
+										<p className="text-black text-sm 2xl:text-base 3xl:text-xl">
 											<span>Jangan lewatkan kesempatan daftar </span>
 											<br className="hidden xl:block" />
 											program bimbingan belajar tanpa&nbsp;
@@ -105,7 +106,7 @@ const Home = () => {
 							<div className="h-full bg-purple-500x">
 								<div className="flex items-center bg-red-500x h-full bg-yellow-500x">
 									<div className="flex-1 bg-green-500x lg:max-w-xl lg:mx-auto lg:text-center xl:text-left">
-										<h1 className="text-4xl 2xl:text-5xl leading-snug bg-yellow-500x font-semibold">
+										<h1 className="text-4xl 2xl:text-5xl 3xl:text-6xl leading-snug bg-yellow-500x font-semibold">
 											Ajak Teman dan <br className="hidden xl:block" />
 											Dapatkan
 											<br className="hidden xl:block" />
@@ -114,7 +115,7 @@ const Home = () => {
 										<div className="flex justify-start lg:justify-center xl:justify-start">
 											<div className="h-1 w-40 bg-gray-900 mt-4" />
 										</div>
-										<p className="text-black text-sm mt-4 2xl:text-base">
+										<p className="text-black text-sm mt-4 2xl:text-base 3xl:text-xl">
 											Bagikan&nbsp;pengalaman&nbsp;
 											<br className="hidden xl:block" />
 											positifmu&nbsp;dan&nbsp;ajak&nbsp;
@@ -127,7 +128,7 @@ const Home = () => {
 									<div className="bg-red-400z hidden xl:block">
 										<img
 											src={HeroImage3}
-											className="w-[600px] 2xl:w-[800px] "
+											className="w-[600px] 2xl:w-[800px] 3xl:w-[900px] "
 											style={{ borderRadius: '2rem' }}
 										/>
 									</div>
