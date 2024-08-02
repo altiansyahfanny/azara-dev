a67114218223656d1a53ed361e9d38a2c88dfe3e
Altiansyah Fanny <altiansyahf.nutech@gmail.com>
2024-08-02 21:10:16 +0700

    add form-lib component

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 // Production URL
-// export const BASE_URL = 'https://azara-course.vercel.app/api';
+export const BASE_URL = 'https://azara-course.vercel.app/api';
 
 // Development URL
-export const BASE_URL = 'https://azara-web-be-dev.vercel.app/api';
+// export const BASE_URL = 'https://azara-web-be-dev.vercel.app/api';
